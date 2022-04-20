cb4a08008ddbb70d8061504eb028b7b12082dc94
diaohancai <diaohancai@cvte.com>
2022-04-20 16:10:15 +0800

    springboot init

---
+++ src/main/java/com/dhc/hadoop3/Hadoop3Application.java
@@ -0,0 +1,13 @@
+package com.dhc.hadoop3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hadoop3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hadoop3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhc/hadoop3/Hadoop3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhc.hadoop3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hadoop3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
